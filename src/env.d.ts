af8efbc0a153592d5a82418bfe7a420ac624a414
Vigen <vigenpooya@gmail.com>
2022-01-08 19:27:32 +0330

    add new envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,15 @@ declare namespace NodeJS {
   interface ProcessEnv {
     SERVER: string;
     DATABASE_HOST: string;
+    PORT: string;
+    DATABASE_HOST: string;
     DATABASE_USER: string;
     DATABASE_NAME: string;
     DATABASE_PASS: string;
     JWT_SECRET: string;
     JWT_REFRESH_SECRET: string;
     COOKIE_SECRET: string;
+    PAGINATE_LIMIT: string;
+    CACHE_EXPIRATION_TIME: string;
   }
 }
