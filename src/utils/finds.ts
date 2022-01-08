ec8525a43960136446aabdae82c6d3f119e7b22a
Vigen <vigenpooya@gmail.com>
2022-01-08 21:18:41 +0330

    refactor

--- src/utils/finds.ts
+++ src/utils/finds.ts
@@ -6,40 +6,41 @@ import { CACHE_EXPIRATION_TIME, PAGINATE_LIMIT } from '../constants'
 import { fieldsToRelationsArgumentable } from './fieldsToRelations'
 import { fieldsProjection } from 'graphql-fields-list'
 
-const checkKey = (key: string) => {
-  const convetableKeys = [
-    'and',
-    'or',
-    'not',
-    'eq',
-    'ne',
-    'in',
-    'nin',
-    'gt',
-    'gte',
-    'lt',
-    'lte',
-    'like',
-    're',
-    'ilike',
-    'overlap',
-    'contains',
-    'contained'
-  ]
+const CONVERTABLE_FILTER_KEYS = [
+  'and',
+  'or',
+  'not',
+  'eq',
+  'ne',
+  'in',
+  'nin',
+  'gt',
+  'gte',
+  'lt',
+  'lte',
+  'like',
+  're',
+  'ilike',
+  'overlap',
+  'contains',
+  'contained'
+]
+const COUNT_NEEDABLE_KEYS = ['meta.pages', 'meta.total']
 
-  if (convetableKeys.includes(key)) return '$' + key
+const checkFiltersKey = (key: string) => {
+  if (CONVERTABLE_FILTER_KEYS.includes(key)) return '$' + key
   return key
 }
 
-const checkArray = (array: any[]): any[] => {
-  return array.map((item) => (isArray(item) ? checkArray(item) : isObject(item) ? findWhere(item) : item))
+const checkFiltersArray = (array: any[]): any[] => {
+  return array.map((item) => (isArray(item) ? checkFiltersArray(item) : isObject(item) ? findWhere(item) : item))
 }
 
 export const findWhere = (filters: any = {}): any =>
   Object.fromEntries(
     Object.keys(filters).map((key) => {
       const value = filters[key]
-      return [checkKey(key), isArray(value) ? checkArray(value) : isObject(value) ? findWhere(value) : value]
+      return [checkFiltersKey(key), isArray(value) ? checkFiltersArray(value) : isObject(value) ? findWhere(value) : value]
     })
   )
 
@@ -48,13 +49,6 @@ export const findOrderBy = (orderBy: any): any => {
     return Object.fromEntries(orderBy?.map((order) => [order.field, order.query]))
   }
   return undefined
-}
-
-export const findPagination = (pagination: any) => {
-  const limit = pagination?.limit || PAGINATE_LIMIT
-  const page = (pagination?.page || 0) - 1 <= 0 ? 0 : (pagination?.page || 0) - 1
-  const offset = pagination?.offset || page * limit
-  return { limit, offset, cache: CACHE_EXPIRATION_TIME }
 }
 
 export const findWithOptions = async <T extends AnyEntity<T>, P extends Populate<T> = any>(
@@ -71,7 +65,7 @@ export const findWithOptions = async <T extends AnyEntity<T>, P extends Populate
 
   let data: T[] | (undefined & any) = []
   let meta: MetaResponseCollection = { page: page + 1, limit }
-  const needCount = Object.keys(fieldsProjection(info)).includes('meta.pages') || Object.keys(fieldsProjection(info)).includes('meta.total')
+  const isNeedingCount = !!Object.keys(fieldsProjection(info)).find((key) => COUNT_NEEDABLE_KEYS.includes(key))
 
   const FindWhere = merge(where, findWhere(args.filters)) as FilterQuery<T>
   const FindOptions = {
@@ -82,9 +76,9 @@ export const findWithOptions = async <T extends AnyEntity<T>, P extends Populate
     populate: fieldsToRelationsArgumentable(info, includes) as any
   } as FindOptions<T, P>
 
-  if (needCount) {
+  if (isNeedingCount) {
     const [response, count] = await em.findAndCount(entityName, FindWhere, FindOptions)
-    meta.pages = Math.ceil(count / (FindOptions.limit || PAGINATE_LIMIT))
+    meta.pages = Math.ceil(count / limit)
     meta.total = count
     data = response
   } else {
